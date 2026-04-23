4bbd05d703f7e87891edeb37d83ae9d75d28198d
Shawn Roman <avahaulick@gmail.com>
2026-04-23 05:28:22 -0600

    Require name in signup validation and remove duplicate custom errors

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -28,9 +28,10 @@ module.exports.validateCardBody = celebrate({
 
 module.exports.validateUserBody = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).messages({
+    name: Joi.string().required().min(2).max(30).messages({
       "string.min": 'The minimum length of the "name" field is 2',
       "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
     }),
     avatar: Joi.string().required().custom(validateURL).messages({
       "string.empty": 'The "avatar" field must be filled in',
